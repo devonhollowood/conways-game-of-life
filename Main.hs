776e5d4fa1094eaa6f862f480ae70597e85b0daa
Devon Hollowood <devonhollowood@gmail.com>
2015-06-01 15:14:48 -0700

    A finished version of Conway's Game of Life

--- Main.hs
+++ Main.hs
@@ -2,35 +2,78 @@ import System.Environment (getArgs)
 import Graphics.Gloss
 import GameOfLife
 import Graphics.Gloss.Interface.Pure.Simulate
+import Text.Parsec
+import Text.Parsec.String (Parser)
 
-cellWidth  = 30 :: Int
-cellHeight = 30 :: Int
+cellWidth  = 10 :: Int
+cellHeight = 10 :: Int
 
 main = do
-    [filename] <- getArgs
+    [speed,filename] <- getArgs
     grid <- parseFile filename
-    let xpix = cellWidth * nrows grid
-        ypix = cellHeight * ncols grid
+    let xpix = cellWidth * ncols grid
+        ypix = cellHeight * nrows grid
     simulate
            (InWindow "Conway's Game of Life" (xpix, ypix) (10, 10))
            white
-           1
+           (read speed)
            grid
-           gridPic
+           (gridPic xpix ypix)
            iter
 
 parseFile :: String -> IO Grid
-parseFile _ = return $ makeGrid 6 6 [(2,3), (2,4), (2,5), (3,2), (3,3), (3,4)]
+parseFile file = do
+    contents <- readFile file
+    case parse pFile "" contents of
+        Right g -> return g
+        Left e  -> error (show e)
 
-gridPic :: Grid -> Picture
-gridPic grid = pictures (fmap make_cell $ cellList grid)
-        where make_cell c = color (if cellState c == Live then red else white)
-                            $ translate
-                                (fromIntegral $ cellWidth * cellRow c)
-                                (fromIntegral $ cellHeight*cellCol c)
-                            $ rectangleSolid
-                                (fromIntegral cellWidth)
-                                (fromIntegral cellHeight)
+pFile :: Parser Grid
+pFile = do
+    many space
+    width <- pInt
+    many space
+    height <- pInt
+    many space
+    liveCells <- pCell `sepEndBy` many1 space
+    eof
+    return $ makeGrid width height liveCells
+
+pInt :: Parser Int
+pInt = many1 digit >>= return . read
+
+pCell :: Parser (Int, Int)
+pCell = do
+    char '('
+    x <- pInt
+    char ','
+    optional $ char ' '
+    y <- pInt
+    char ')'
+    return (x,y)
+
+gridPic :: Int -> Int -> Grid -> Picture
+gridPic xpix ypix grid = pictures $ fmap (makeCell xpix ypix) (cellList grid)
+
+makeCell xpix ypix cell = uncurry translate (position xpix ypix cell) $
+                              if cellState cell == Live
+                              then liveCell
+                              else deadCell
+
+position :: Int -> Int -> Cell -> (Float, Float)
+position xpix ypix cell =
+    ((fromIntegral (cellWidth * cellCol cell) - xoffset),
+     negate (fromIntegral (cellHeight * cellRow cell) - yoffset))
+    where xoffset = fromIntegral (xpix-cellWidth) / 2.0
+          yoffset = fromIntegral (ypix-cellWidth) / 2.0
+
+liveCell :: Picture
+liveCell = color blue $
+    rectangleSolid (fromIntegral cellWidth) (fromIntegral cellHeight)
+
+deadCell :: Picture
+deadCell = color (black) $
+    rectangleWire (fromIntegral cellWidth) (fromIntegral cellHeight)
 
 iter :: ViewPort -> Float -> Grid -> Grid
 iter _ _ grid = updateGrid grid
